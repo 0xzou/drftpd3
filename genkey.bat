ad47037037793a6bf892c7dd880089f58ace64b5
0xzou <hol4nd4@gmail.com>
2020-05-14 15:29:46 +0100

    Dirty fix in order to include drftpd.key in slave.zip (Windows)

--- genkey.bat
+++ genkey.bat
@@ -1,3 +1,5 @@
+@echo off
+
 set ROOT=%~dp0
 set USERDATA=%ROOT%userdata\
 if not exist "%USERDATA%" (
@@ -7,4 +9,16 @@ if not exist "%USERDATA%" (
     goto :eof
   )
 )
+
 keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "%USERDATA%drftpd.key" -storetype pkcs12 -storepass drftpd
+
+set SLAVEUSERDATA=%ROOT%src\slave\resources\userdata\
+if not exist "%SLAVEUSERDATA%" (
+  mkdir "%SLAVEUSERDATA%"
+  if "%errorlevel%" NEQ "0" (
+    echo Error while creating userdata folder %SLAVEUSERDATA% for slave.zip
+    goto :eof
+  )
+)
+
+xcopy %USERDATA%* %SLAVEUSERDATA%*
